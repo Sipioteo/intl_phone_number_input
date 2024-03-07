9f19abcd339cb42648f741589fc5dded1cd3fd5a
HamadaAllipy <hamada.devlop@gmail.com>
2024-03-07 03:07:53 +0200

    finish login for hint text

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,13 @@
 import 'dart:async';
 import 'dart:ui';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+import 'package:mask/mask.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 void main() => runApp(MyApp());
 
@@ -24,6 +26,7 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(title: Text('Demo')),
           // body: AbberApp(),
           body: MyTextField(),
+          // body: PinCodeVerificationScreen(),
         ),
       ),
     );
@@ -168,260 +171,135 @@ class MyTextField extends StatefulWidget {
 }
 
 class _MyTextFieldState extends State<MyTextField> {
-  late final TextEditingController _controller;
-
-  String _hintText = '';
-
-  int maxDigit = 10;
-  late List<String> _remainsZero;
-  String previousText = '';
+  late final TextEditingController _controller2;
+  String _hintText2 = '';
 
   @override
   void initState() {
-    String input = "1234567890123456";
-    String format = "00 0000 0000 0000";
-
-    String formattedText = applyFormat(input, format);
-    print(formattedText); // Output: "1234 5678 9012 3456"
-
-
-
-    _remainsZero = List.generate(maxDigit, (index) => '0').toList();
-    _controller = TextEditingController();
-    _controller.addListener(_listener);
-    String finalStr = _remainsZero.reduce((value, element) {
-      return value + element;
-    });
-    _hintText = finalStr;
-
+    _controller2 = TextEditingController();
+    _getFormatHintText(formatHint);
+    AppFormatter.mask = formatHint;
     super.initState();
-  }
-
-
-  void _listener() {
-    String currentText = _controller.text.replaceAll('  ', '');
-    if(currentText.length > maxDigit){
-      return;
-    }
-    else if (previousText.length > currentText.length) {
-      // Backspace pressed
-      _remainsZero
-          .replaceRange(currentText.length, currentText.length + 1, ['0']);
-    } else if (previousText.length < currentText.length) {
-      // type a new char
-      _remainsZero.replaceRange(
-          currentText.length - 1, currentText.length, [''.padLeft(1)]);
-    }
-
-     if(currentText.length <= maxDigit){
-      String finalStr = _remainsZero.reduce((value, element) {
-        return value + element;
-      });
-
-      setState(() {
-        _hintText = finalStr;
-      });
-      previousText = currentText;
-    }
-
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _controller2.dispose();
     super.dispose();
   }
 
+  String formatHint = '# ### ## #### ##';
+
+  void _getFormatHintText(String input) {
+    String replacedAll = input.replaceAll('#', '0');
+    setState(() {
+      _hintText2 = replacedAll;
+    });
+  }
+
+  String previousText2 = '';
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        margin: EdgeInsets.all(16),
-        padding: EdgeInsets.symmetric(horizontal: 8),
-        // color: Colors.red.withOpacity(0.4),
-        decoration: BoxDecoration(
-          border: Border.all(),
-        ),
-        height: 50,
-        child: Stack(
-          children: [
-            // TextFormField(
-            //   keyboardType: TextInputType.number,
-            //   textDirection: TextDirection.ltr,
-            //   enabled: false,
-            //   textAlign: TextAlign.left,
-            //   style: TextStyle(
-            //     fontSize: 18,
-            //     letterSpacing: 1.7,
-            //   ),
-            //   onTapOutside: (event) =>
-            //       FocusManager.instance.primaryFocus?.unfocus(),
-            //   decoration: InputDecoration(
-            //       hintText: _hintText,
-            //       hintStyle: TextStyle(
-            //         fontSize: 18,
-            //         letterSpacing: 2,
-            //       ),
-            //       suffixText: ''.padLeft(
-            //           _controller.text.replaceAll('  ', '').length * 2),
-            //       border: InputBorder.none,
-            //       contentPadding: EdgeInsets.zero),
-            // ),
-            TextFormField(
-              keyboardType: TextInputType.number,
-              textDirection: TextDirection.ltr,
-              controller: _controller,
-              textAlign: TextAlign.left,
-              maxLengthEnforcement: MaxLengthEnforcement.none,
-              inputFormatters: [
-                DynamicCardNumberInputFormatter('0 00 000 0000 000000'),
-              ],
-              style: TextStyle(
-                fontSize: 18,
-                letterSpacing: -2,
-                // backgroundColor: Colors.red,
-              ),
-              onTapOutside: (event) =>
-                  FocusManager.instance.primaryFocus?.unfocus(),
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.zero,
-              ),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Center(
+        child: InputDecorator(
+          isEmpty: true,
+          textAlign: TextAlign.left,
+          decoration: InputDecoration(
+            hintText: _hintText2,
+            hintStyle: Theme.of(context).textTheme.titleMedium?.copyWith(
+              color: Colors.grey,
+              fontFeatures: [FontFeature.tabularFigures()],
             ),
-          ],
+            hintTextDirection: TextDirection.ltr,
+            border: InputBorder.none,
+          ),
+          child: TextField(
+            keyboardType: TextInputType.number,
+            textAlign: TextAlign.left,
+            controller: _controller2,
+            textDirection: TextDirection.ltr,
+            style: Theme.of(context).textTheme.titleMedium?.copyWith(
+              fontFeatures: [FontFeature.tabularFigures()],
+            ),
+            inputFormatters: [
+              AppFormatter.maskFormatter,
+            ],
+            onChanged: (value) {
+              final baseOffset = _controller2.selection.baseOffset;
+              if (value.length < previousText2.length) {
+                String result = _hintText2;
+                List<int> spaceIndices = [];
+                int currentIndex = previousText2.indexOf(' ');
+                while (currentIndex != -1) {
+                  spaceIndices.add(currentIndex);
+                  currentIndex = previousText2.indexOf(' ', currentIndex + 1);
+                }
+                // print("Index of space: $spaceIndices");
+                // print("baseOffset: $baseOffset");
+                if(spaceIndices.isEmpty){
+                  result = _hintText2.replaceRange(
+                      baseOffset, baseOffset + 1, '0');
+                  print("Replace first");
+                }
+                else if(spaceIndices.last != baseOffset){
+                  print("Replace");
+                  result = _hintText2.replaceRange(
+                      baseOffset, baseOffset + 1, '0');
+                }
+                else {
+                  print("Skip");
+                  result = _hintText2.replaceRange(
+                            baseOffset + 1, baseOffset + 2, '0');
+
+                }
+                // String result = _hintText2;
+                // check where index of space
+                // for(var char in previousText2.split('')){
+                //   if (char == ' ') {
+                //     print('SKIP OVER SPACE ');
+                //
+                //     result = _hintText2.replaceRange(
+                //         baseOffset + 1, baseOffset + 2, '0');
+                //     break;
+                //   } else {
+                //     print('REPLACE ');
+                //
+                //     result = _hintText2.replaceRange(
+                //         baseOffset, baseOffset + 1, '0');
+                //   }
+                //
+                // }
+                _hintText2 = result;
+              }
+
+              else {
+                String result = _hintText2.replaceRange(
+                    baseOffset - 1, baseOffset, value.split('').last);
+                _hintText2 = result;
+              }
+              previousText2 = value;
+              setState(() {});
+            },
+          ),
         ),
       ),
     );
   }
-
-
-
 }
 
+class AppFormatter {
+  static String mask = '';
 
-class CardNumberInputFormatter extends TextInputFormatter {
-  @override
-  TextEditingValue formatEditUpdate(
-      TextEditingValue oldValue, TextEditingValue newValue) {
-    var text = removeArabicNumbers(newValue.text);
+  static TextInputFormatter maskFormatter = MaskTextInputFormatter(
+      mask: mask,
+      filter: {"#": RegExp(r'[0-9]')},
+      type: MaskAutoCompletionType.lazy);
 
-    if (newValue.selection.baseOffset == 0) {
-      return newValue;
-    }
-
-    var formattedText = _formatCardNumber(text);
-    return TextEditingValue(
-      text: formattedText,
-      selection: TextSelection.collapsed(offset: formattedText.length),
-    );
-  }
-
-  String _formatCardNumber(String input) {
-    input = input.replaceAll(RegExp(r'\D'), ''); // Remove non-digits
-    var buffer = StringBuffer();
-    for (int i = 0; i < input.length; i++) {
-      buffer.write(input[i]);
-      var nonZeroIndex = i + 1;
-      if (nonZeroIndex % 1 == 0 && nonZeroIndex != input.length) {
-        // Add double spaces
-        buffer.write('  ');
-      }
-    }
-    return buffer.toString();
-  }
-}
-
-class DynamicCardNumberInputFormatter extends TextInputFormatter {
-  final String format;
-
-  DynamicCardNumberInputFormatter(this.format);
-
-  @override
-  TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
-    var text = newValue.text.replaceAll(RegExp(r'\D'), ''); // Remove non-digits
-
-    if (newValue.selection.baseOffset == 0) {
-      return newValue;
-    }
-    var formattedText = _formatText(text);
-    return TextEditingValue(
-      text: formattedText,
-      selection: TextSelection.collapsed(offset: formattedText.length),
-    );
-  }
-
-
-  String _formatText(String input) {
-    var buffer = StringBuffer();
-    int formatIndex = 0;
-
-    for (int i = 0; i < input.length; i++) {
-      // Skip over spaces in the format string
-      if (i > 0) {
-        buffer.write(' ');
-      }
-
-      // Append the input character
-      while (formatIndex < format.length && format[formatIndex] == ' ') {
-        buffer.write('  ');
-        formatIndex++;
-      }
-
-      if (i >= input.length) {
-        break;
-      }
-
-      buffer.write(input[i]);
-      formatIndex++;
-    }
-
-    return buffer.toString();
-  }
-
-
-
-}
-
-
-
-
-String removeArabicNumbers(String text) => text
-    .replaceAll('١', '1')
-    .replaceAll('٢', '2')
-    .replaceAll('٣', '3')
-    .replaceAll('٤', '4')
-    .replaceAll('٥', '5')
-    .replaceAll('٦', '6')
-    .replaceAll('٧', '7')
-    .replaceAll('٨', '8')
-    .replaceAll('٩', '9')
-    .replaceAll('٠', '0');
-
-
-String applyFormat(String input, String format) {
-  String formattedText = '';
-  int count = 0;
-
-  for (int i = 0; i < input.length; i++) {
-    if (count >= format.length) {
-      break; // Exit loop if we reach the end of the format string
-    }
-
-    if (format[count] == '0') {
-      formattedText += input[i];
-    } else {
-      formattedText += format[count]; // Add the format character
-      formattedText += input[i]; // Add the input character
-    }
-
-    count++;
-
-    // Add a space if the next character in the format is a space
-    if (count < format.length && format[count] == ' ') {
-      formattedText += ' ';
-      count++;
-    }
-  }
-
-  return formattedText;
+  static TextInputFormatter maskFormatter2 = Mask.generic(
+    masks: [mask],
+    hashtag: Hashtag.numbers,
+  );
 }
